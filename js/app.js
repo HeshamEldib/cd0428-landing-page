6bf1bcff5f2be8ceea6aac51c55ddc4fa3108432
Hesham <heshamaldiyb@gmail.com>
2023-01-10 16:39:26 +0200

    Ubdate The Project

--- js/app.js
+++ js/app.js
@@ -1,54 +1,55 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
+* 
+* Manipulating the DOM exercise.
+* Exercise programmatically builds navigation,
+* scrolls to anchors from navigation,
+* and highlights section in viewport upon scrolling.
+* 
+* Dependencies: None
+* 
+* JS Version: ES2015/ES6
+* 
+* JS Standard: ESlint
+* 
 */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+* Comments should be present at the beginning of each procedure and class.
+* Great to have comments before crucial code sections within the procedure.
 */
 
 /**
- * Define Global Variables
- * 
+* Define Global Variables
+* 
 */
 
 
 /**
- * End Global Variables
- * Start Helper Functions
- * 
+* End Global Variables
+* Start Helper Functions
+* 
 */
 
 
 
 /**
- * End Helper Functions
- * Begin Main Functions
- * 
+* End Helper Functions
+* Begin Main Functions
+* 
 */
 
 // build the nav
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 
-const nav = document.querySelector("#navbar__list");
+const nav = document.querySelector('#navbar__list');
 sections.forEach((e) => {
-    const list = document.createElement("li");
-    const link = document.createElement("a");
+    const list = document.createElement('li');
+    const link = document.createElement('a');
     const linkContent = document.createTextNode(e.dataset.nav);
-
-    link.setAttribute("href", '#' + e.id);
+// ###########################
+    link.setAttribute('href', '#');
     link.classList.add(e.id);
+    link.classList.add('menu__link');
 
     link.appendChild(linkContent);
     list.appendChild(link);
@@ -57,22 +58,22 @@ sections.forEach((e) => {
 
 // Add class 'active' to section when near top of viewport
 function makeActive(){
-    for (let section of sections) {
+    for (const section of sections) {
         const box = section.getBoundingClientRect();
         if (box.top <= 150 && box.bottom >= 150) {
-            section.classList.add("active");
+            section.classList.add('active');
             const link = document.querySelector(`#navbar__list a.${section.id}`);
-            link.classList.add("active");
+            link.classList.add('active');
         } else {
-            section.classList.remove("active");
+            section.classList.remove('active');
             document.querySelectorAll(`#navbar__list a.${section.id}`).forEach((e) => {
-                e.classList.remove("active");
+                e.classList.remove('active');
             });
         }
     }
 }
 
-document.addEventListener("scroll", function() {
+document.addEventListener('scroll', function() {
     makeActive();
 });
 
@@ -80,24 +81,22 @@ document.addEventListener("scroll", function() {
 
 
 /**
- * End Main Functions
- * Begin Events
- * 
+* End Main Functions
+* Begin Events
+* 
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
-const links = document.querySelectorAll("#navbar__list li a");
+const links = document.querySelectorAll('#navbar__list li a');
 links.forEach((li) => {
-    li.addEventListener("click", (e) => {
-        let topSection = document.querySelector("#" + e.target.classList[0]).offsetTop;
-        window.scrollTo(0, topSection);
-    })
-})
+    li.addEventListener('click', (e) => {
+        const topSection = document.querySelector('#' + e.target.classList[0]);
+        topSection.scrollIntoView({behavior: 'smooth'});
+        e.preventDefault();
+    });
+});
 
 // Set sections as active
-
-
-
 
